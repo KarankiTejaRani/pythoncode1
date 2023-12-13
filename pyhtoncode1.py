4092238a48feccca899ca2d9f5ddb291c72cd32a
KarankiTejaRani <116069544+KarankiTejaRani@users.noreply.github.com>
2023-12-13 16:08:35 +0530

    Add files via upload

---
+++ pyhtoncode1.py
@@ -0,0 +1,8 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> weight_kg = 54
+>>> height_meter = 5
+>>> height_squared = height_meter * height_meter
+>>> BMI = weight_kg/height_squared
+>>> print("BMI : ", round(BMI))
+BMI :  2
